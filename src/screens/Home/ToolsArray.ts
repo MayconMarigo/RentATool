4e49f1ff0a4065cf0cf890bcdb89ccd93e8003e1
Maycon Marigo <maycon.marigo@elo.com.br>
2023-05-31 23:32:41 -0300

    adicionar itens dentro de ferramentas

---
+++ src/screens/Home/ToolsArray.ts
@@ -0,0 +1,44 @@
+import { ITools } from "./Home";
+import Britadeira from "../../assets/britadeira.png"
+import Compactador from "../../assets/compactador.png";
+import Furadeira from "../../assets/furadeira.png";
+import LixaDiagonal from "../../assets/lixa-diagonal.png"
+import Lixadeira from "../../assets/lixadeira.png"
+import Parafusadeira from "../../assets/parafusadeira.png"
+import SerraTicoTico from "../../assets/serra-tico-tico.png"
+import Trena from "../../assets/trena.png"
+
+export const toolsArray: ITools[] = [
+  {
+    id: 0,
+    img: Britadeira,
+  },
+  {
+    id: 1,
+    img: Compactador,
+  },
+  {
+    id: 2,
+    img: Furadeira,
+  },
+  {
+    id: 3,
+    img: LixaDiagonal,
+  },
+  {
+    id: 4,
+    img: Lixadeira,
+  },
+  {
+    id: 5,
+    img: Parafusadeira,
+  },
+  {
+    id: 6,
+    img: SerraTicoTico,
+  },
+  {
+    id: 7,
+    img: Trena,
+  },
+];
